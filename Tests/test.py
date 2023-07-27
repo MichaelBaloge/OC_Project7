6d8be90276c297e57373db035f8d571df9890bb4
MichaelBaloge <139211557+MichaelBaloge@users.noreply.github.com>
2023-07-27 09:30:07 +0200

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -3,7 +3,7 @@ import requests
 
 @pytest.fixture
 def api_url():
-    return 'http://127.0.0.1:5000/predict'
+    return 'https://mbcreditmodelapi.azurewebsites.net/predict'
 
 def test_prediction_accept(api_url):
     data = {'data':100001}
